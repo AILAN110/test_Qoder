451c924658a5289b8045dc47899be3ffc9c9778c
AILAN110 <1032163939@qq.com.com>
2026-06-09 17:40:04 +0800

    ss

--- test.py
+++ test.py
@@ -13,7 +13,29 @@ def quick_sort(arr):
     return quick_sort(left) + middle + quick_sort(right)
 
 
+def bubble_sort_v2(arr):
+    """
+    Return a sorted copy of arr using an optimized bubble sort algorithm.
+    """
+    sorted_arr = arr.copy()
+    n = len(sorted_arr)
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - i - 1):
+            if sorted_arr[j] > sorted_arr[j + 1]:
+                sorted_arr[j], sorted_arr[j + 1] = sorted_arr[j + 1], sorted_arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return sorted_arr
+
+
 if __name__ == "__main__":
     test_array = [64, 34, 25, 12, 22, 11, 90]
     print("Original array:", test_array)
-    print("Sorted array:", quick_sort(test_array))
+    print("Quick sorted array:", quick_sort(test_array))
+    print("Bubble sorted array:", bubble_sort_v2(test_array))
