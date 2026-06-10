138684735f1dfc16708b2e28c9a4f5dd99f55b16
AILAN110 <1032163939@qq.com.com>
2026-06-10 09:19:53 +0800

    sss

--- test_vscode.py
+++ test_vscode.py
@@ -1,1 +1,18 @@
-print("hello world")
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+def test():
+    print("测试")
+
+if __name__ == "__main__":
+    test_array = [64, 34, 25, 12, 22, 11, 90]
+    print("原始数组:", test_array)
+    sorted_array = quick_sort(test_array)
+    print("排序后数组:", sorted_array)
+    test()
