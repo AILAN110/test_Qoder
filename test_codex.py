86c0598af44b36b14e49c10bc157f56cda546e7a
AILAN110 <1032163939@qq.com.com>
2026-06-10 09:51:25 +0800

    sad

--- test_codex.py
+++ test_codex.py
@@ -1,1 +1,19 @@
 print("")
+
+
+def bubble_sort(items):
+    """Return a new list sorted in ascending order using bubble sort."""
+    result = list(items)
+    length = len(result)
+
+    for i in range(length):
+        swapped = False
+        for j in range(0, length - i - 1):
+            if result[j] > result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return result
