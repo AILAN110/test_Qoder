b1dad1410f2f845fcc64b893b658d88508d9b01c
AILAN110 <1032163939@qq.com.com>
2026-06-11 15:12:27 +0800

    xxx

--- test_opencode.py
+++ test_opencode.py
@@ -1,3 +1,10 @@
+import random
+
+
+def generate_random_array(length=10, min_val=0, max_val=100):
+    return [random.randint(min_val, max_val) for _ in range(length)]
+
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
